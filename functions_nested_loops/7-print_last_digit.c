5a65018ddf48adc743fe1a96dec6170b5d471dc5
root <seth.rueda@gmail.com>
2024-02-19 23:35:07 +0000

    ohnocringe

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - modulo10
+ * @n: int
+ * Return: m
+ */
+int print_last_digit(int n)
+{
+	int m = n % 10;
+
+	if (m < 0)
+		m = -m;
+	_putchar(m + '0');
+	return (0);
+}
