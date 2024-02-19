3744fe35efe3d7accb00ef21c9f57ee9c2451144
root <seth.rueda@gmail.com>
2024-02-19 15:01:00 +0000

    hexaprint

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - compte en base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+	
+	int j = 97;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 103)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
