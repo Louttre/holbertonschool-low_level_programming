47d2ac2537c1dff8b9dd75f87ea0a3dfc5510790
root <seth.rueda@gmail.com>
2024-02-19 14:18:48 +0000

    base100

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - compte en base 10
+ * return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
