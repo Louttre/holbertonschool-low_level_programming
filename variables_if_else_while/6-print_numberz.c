96d013a2e4c98e21c546c749c084ee8db9cb8cd1
root <seth.rueda@gmail.com>
2024-02-19 14:54:04 +0000

    putchar0-9

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - compte en base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
