9ffab1d7b8d2ca0378ded84a35ec286cbd2dda8a
root <seth.rueda@gmail.com>
2024-02-19 16:38:02 +0000

    letstry

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - print _putchar
  * Return: 0
  */
-int _putchar(void)
-{
-	printf("_putchar\n");
-	return (0);
-}
-
 int main(void)
 {
     return _putchar();
+}
+
+int _putchar(void)
+{
+        printf("_putchar\n");
+        return (0);
 }
