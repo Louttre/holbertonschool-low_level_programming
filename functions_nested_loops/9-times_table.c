155aed59c283b8da473c9d3d67c9cdd91ed873da
root <seth.rueda@gmail.com>
2024-02-20 08:45:16 +0000

    aze

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+void times_table(void)
+{
+	int n = 0;
+	
+	while (n < 10)
+	{
+		int i = 0;
+
+		while (i < 10)
+		{
+			_putchar((i * n) + '0');
+			if (i != 9)
+			{	
+				_putchar(',');
+				_putchar(' ');
+			}
+			i++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char a);
+char _putchar(char a);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -10,4 +10,5 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
 #endif
