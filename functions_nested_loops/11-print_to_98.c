99152401cbbbba79156766a3c01908e45da3fd7d
root <seth.rueda@gmail.com>
2024-02-20 11:16:58 +0000

    ahah

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_to_98 - print to 98
+ * @n: valeur entr√
+ */
 void print_to_98(int n)
 {
 	int i = n;
+
 	if (n < 98)
 	{
 		while (i < 98)
@@ -13,7 +17,7 @@ void print_to_98(int n)
 		}
 	}
 	else
-	{	
+	{
 		while (i > 98)
 		{
 			printf("%d, ", i);
