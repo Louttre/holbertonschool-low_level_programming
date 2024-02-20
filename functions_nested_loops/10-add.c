c426c8d9b43b380ce6d22e5d6f8dd2007adc390b
root <seth.rueda@gmail.com>
2024-02-20 09:40:31 +0000

    finalv

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,8 +1,13 @@
 #include "main.h"
-
-
+/**
+ * add - ajoute deux int
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: rez
+ */
 int add(int a, int b)
 {
 	int rez = a + b;
-	return rez;
+
+	return (rez);
 }
