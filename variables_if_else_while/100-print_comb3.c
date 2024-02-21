477a83c94f0dd13216ba575ff134e4dd3027ce8c
root <seth.rueda@gmail.com>
2024-02-21 10:12:43 +0000

    ez

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - 00 to 99 w/o rep
+ * Return: 0
+ */ 
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 57)
+	{
+		int j = 48;
+
+		while (j < 58)
+		{
+			while (j <= i)
+			{	
+				j++;
+			}
+			putchar(i);
+			putchar(j);
+			if (i < 56)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
