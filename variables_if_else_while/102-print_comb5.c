528dddd48be3b2422b406718e265d1e9263d0aaf
root <seth.rueda@gmail.com>
2024-02-22 10:07:53 +0000

    finally

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
-
+/**
+ * main - compteur
+ * Return: 0
+ */
 int main(void)
 {
 	int i = 0;
 
-	for(; i < 100; i++)
+	for(;i < 100; i++)
 	{
 		int j = i + 1;
 
-		for (; j < 100; j++)
-		{		
+		for (;j < 100; j++)
+		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
-			if (j == 99 && i == 98) 
+			if (j == 99 && i == 98)
 				putchar('\n');
 			else
-			{	
+			{
 				putchar(',');
 				putchar(' ');
-	
+
 			}
 		}
 	}
 	return (0);
-}	
+}
