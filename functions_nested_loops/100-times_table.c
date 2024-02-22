ad8b2f4404fd89562aea83f4e0dc7dcb20bc3410
root <seth.rueda@gmail.com>
2024-02-22 15:41:29 +0000

    taimetable

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * print_times_table - print tableau
+ */
 void print_times_table(int n)
 {
 	int i = 0;
-	
+
 	if ( n < 0 || n > 14)
 		return;
 	for (; i <= n; i++)
 	{
 		int j = 1;
-		
+
 		_putchar('0');
 		if (n == 0)
-		{	
+		{
 			_putchar('\n');
 			return;
-		}	
+		}
 		_putchar(',');
 
 		for (; j <= n; j++)
-		{	
+		{
 			int k = j * i;
 
 			if (k < 10)
-			{			
+			{		
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			}
 			else if (k < 100 && k > 9)
-			{	
+			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
@@ -50,4 +52,4 @@ void print_times_table(int n)
 				_putchar(',');
 		}
 	}
-}	
+}
