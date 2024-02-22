eb21f9e0f1871d2ff741875fe599a0ba121e356e
root <seth.rueda@gmail.com>
2024-02-22 21:28:56 +0000

    fibo2

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main (void)
+{
+	unsigned long int i = 1;
+
+	unsigned long int j = 2;
+
+	unsigned long int k;
+	
+        unsigned long int compteur = 0;
+
+	while (j < 4000000)
+	{
+		if (j % 2 == 0)
+			compteur += j;
+		k = j;
+		j += i;
+		i = k;
+		if ( j >= 4000000)
+			printf("%lu\n", compteur);
+	}
+	return (0);
+}
