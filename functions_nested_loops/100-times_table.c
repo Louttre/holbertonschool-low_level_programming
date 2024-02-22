7513adb159453ca1bd51445bc6f40d995214f4ae
root <seth.rueda@gmail.com>
2024-02-22 15:32:11 +0000

    taimetable

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -7,41 +7,41 @@ void print_times_table(int n)
 	
 	if ( n < 0 || n > 14)
 		return;
-	for (; i < n + 1; i++)
+	for (; i <= n; i++)
 	{
 		int j = 1;
 
 		printf("0,");
 
-		for (; j < n + 1; j++)
+		for (; j <= n; j++)
 		{	
 			int k = j * i;
 
 			if (k < 10)
 			{			
-				putchar(' ');
-				putchar(' ');
-				putchar(' ');
-				putchar(k + '0');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
 			}
 			else if (k < 100 && k > 9)
 			{	
-				putchar(' ');
-				putchar(' ');
-				putchar((k / 10) + '0');
-				putchar((k % 10) + '0');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 			else if (k < 1000 && k > 99)
 			{
-				putchar(' ');
-				putchar((k / 100) + '0');
-				putchar(((k / 10) % 10) + '0');
-				putchar((k % 10) + '0');
+				_putchar(' ');
+				_putchar((k / 100) + '0');
+				_putchar(((k / 10) % 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 			if (j == n)
-				putchar('\n');
+				_putchar('\n');
 			else
-				putchar(',');
+				_putchar(',');
 		}
 	}
 }	
