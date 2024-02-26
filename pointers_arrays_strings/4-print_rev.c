7e3d289f1be7ae55c0c573aedebcc560063eac29
root <seth.rueda@gmail.com>
2024-02-26 12:05:50 +0000

    UPADATE_pront_rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,12 +4,26 @@
 /**
  * print_rev - reverse a string
  * @s: string
+ * _strlen - count number of case in array
+ * @s: string to count
+ * Return: i
  */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
 void print_rev(char *s)
 {
 	int i;
-
-	for (i = strlen(s); i >= 0; i--)
+	
+	for(i = _strlen(s); i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
