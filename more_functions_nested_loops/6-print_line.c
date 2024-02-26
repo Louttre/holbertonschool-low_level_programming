e3f7b2d2a65b55119328beda23e5f622ebf54c2e
root <seth.rueda@gmail.com>
2024-02-26 07:59:34 +0000

    add_print_lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - print '_' n times
+ * @n: number of '_'
+ */
+void print_line(int n)
+{
+	int i = 0;
+	if (n > 0)
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+	_putchar('\n');
+}
