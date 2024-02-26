31c4935edb93815878637ca8acadc3d9b7799378
Louttre <seth.rueda@gmail.com>
2024-02-26 00:56:21 +0000

    add_print_most_numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - print all digits
+ */
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		if (i == 50 || i == 52)
+			i++;
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}  
