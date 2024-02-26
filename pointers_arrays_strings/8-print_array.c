0d91ddbb2250bec652642df3ad59abb34b964555
root <seth.rueda@gmail.com>
2024-02-26 14:05:07 +0000

    add_print_array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - print n first term of array
+ * @a: array
+ * @n: limit
+ */ 
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i == n - 1)
+			printf("%d\n", a[i]);
+		else
+		{
+			printf("%d, ", a[i]);	
+		}
+		i++;
+	}
+}
