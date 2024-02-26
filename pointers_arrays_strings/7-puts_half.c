f0ea1255aa55f368fa724d6032361685ce069b76
root <seth.rueda@gmail.com>
2024-02-26 13:38:51 +0000

    add_puts_half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - count number of case in array
+ * @s: string to count
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * puts_half - print a char
+ * @str: array of the string
+ */
+void puts_half(char *str)
+{
+	int j = _strlen(str) / 2;
+	if (_strlen(str) % 2 == 1)
+		 j -= 1;
+	while (str[j] != '\0')
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
