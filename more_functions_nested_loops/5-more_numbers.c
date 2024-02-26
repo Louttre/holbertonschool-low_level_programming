d3f882b97396033f7cd92498f3046ed44d7e1390
Louttre <seth.rueda@gmail.com>
2024-02-26 01:18:54 +0000

    add_more_numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+void more_numbers(void)
+{
+	int i,j,k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			_putchar(j);
+		}
+		for (k = 48; k < 53; k++)
+		{
+			_putchar(49);
+			_putchar(k);
+		}
+		printf("\n");
+	}
+}
