5e9de68fd4928eb0942220784bbe3cd564d4924f
root <seth.rueda@gmail.com>
2024-02-26 09:27:43 +0000

    add_print_triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle of size(size)
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	if (size > 0)
+	{	
+		int i;
+
+		for(i = 1; i <= size; i++)
+		{
+			int k = size - i;
+
+			while (k > 0)
+			{
+				_putchar(' ');
+				k--;
+			}
+			while (k < i)
+			{
+				_putchar('#');
+				k++;
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
