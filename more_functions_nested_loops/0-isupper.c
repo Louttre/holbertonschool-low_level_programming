de6c42f87e1b5a19a1513d6bf6b7dd1f6df76ca3
Louttre <seth.rueda@gmail.com>
2024-02-26 00:14:30 +0000

    isupper_function

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - check if c is uppercase
+ * Return: 0 if false
+ */
+int _isupper(int c)
+{
+	if (c < 91 && c > 64)
+		return (1);
+	else
+		return (0);
+}
