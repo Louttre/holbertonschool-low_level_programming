84aac3d270513c51bb001348e06a64cb3215224c
root <seth.rueda@gmail.com>
2024-02-26 14:11:36 +0000

    add_print_array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,15 +8,17 @@
 void print_array(int *a, int n)
 {
 	int i = 0;
-
-	while (i < n)
-	{
-		if (i == n - 1)
-			printf("%d\n", a[i]);
-		else
+	if (n >= 0)
+		while (i < n)
 		{
-			printf("%d, ", a[i]);	
+			if (i == n - 1)
+				printf("%d\n", a[i]);
+			else
+			{
+				printf("%d, ", a[i]);	
+			}
+			i++;
 		}
-		i++;
-	}
+	else
+		printf("_n");
 }
