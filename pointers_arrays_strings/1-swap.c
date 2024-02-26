56d798fa5cb30ddfb294e531dfd30b772f9186d3
root <seth.rueda@gmail.com>
2024-02-26 10:46:56 +0000

    add_swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - swap to int
+ * @a: become b
+ * @b: becoma a
+ */
+void swap_int(int *a, int *b)
+{ 
+	int c = *a ;
+
+	*a = *b;
+	*b = c;
+    			
+}	
