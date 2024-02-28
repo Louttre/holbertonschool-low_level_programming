daa47fb70dea91e3a24081f03da1e0f8cd5b64aa
root <seth.rueda@gmail.com>
2024-02-28 12:51:18 +0000

    add_strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -28,7 +28,7 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; i <= _strlen(s1); i++)
 	{
 		if (s1[i] < s2[i] || s1[i] > s2[i])
-			return (s1[i]-s2[i]);
+			return (s1[i] - s2[i]);
 	}
 	return (0);
 }
