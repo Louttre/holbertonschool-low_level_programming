523db1e56103d906b6a6d24275210cb35f1c7ec2
root <seth.rueda@gmail.com>
2024-02-28 12:50:32 +0000

    add_strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -15,7 +15,12 @@ int _strlen(char *s)
 	}
 	return (i);
 }
-
+/**
+ * _strcmp - compare two string
+ * @s1: first string
+ * @s2: string to compare
+ * Return: 0 if equal or the diff of two diff char
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
