97eb2c169d8bbf0dce28e322218791a23cd0aeda
root <seth.rueda@gmail.com>
2024-02-28 09:49:30 +0000

    add_strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - count number of case in array
+ * @s: string to count
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strcat - fuse two array in one
+ * @dest: destination array
+ * @src: source array
+ * Return: ptr which contain dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+	char *ptr;
+
+	for (i = 0; i <= _strlen(dest); i++)
+	{
+		if (dest[i] == '\0')
+		{
+			while (j < n && src[j] != '\0')
+			{
+				dest[i] = src[j];
+				i++;
+				j++;
+			}
+			dest[i] = '\0';
+		}
+	}
+	ptr = dest;
+	return (ptr);
+}
