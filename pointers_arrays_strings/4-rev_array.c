ad261ec2dc605b11f701a3a6886849df2f0cbf02
root <seth.rueda@gmail.com>
2024-02-28 13:59:55 +0000

    add_rev_array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - count number of case in array
+ * @s: string to count
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * reverse_array - reverse an array
+ * @a: array
+ * @n: number of element
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = n - 1;
+	int temp; 
+
+	while (i <= j)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		j--;
+		i++;
+	}
+}
