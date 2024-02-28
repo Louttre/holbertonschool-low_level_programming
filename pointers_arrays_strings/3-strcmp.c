c62fe9ac643d5d2b6e7a7b08911d28ee646944a3
root <seth.rueda@gmail.com>
2024-02-28 12:48:49 +0000

    add_strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - count number of case in array
+ * @s: string to count
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(s1); i++)
+	{
+		if (s1[i] < s2[i] || s1[i] > s2[i])
+			return (s1[i]-s2[i]);
+	}
+	return (0);
+}
