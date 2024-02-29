f3210efc6f129c2414b7fe5e7dd949b518512140
root <seth.rueda@gmail.com>
2024-02-29 16:26:39 +0000

    UPDATE_leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-/**
- * _strlen - count number of case in array
- * @s: string to count
- * Return: i
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
+#include <string.h>
 /**
  * leet - encrypt the string
  * @a: string to encrypt
@@ -22,19 +8,19 @@ int _strlen(char *s)
  */
 char *leet(char *a)
 {
-	int i;
+	size_t i;
 	char ref[20] = "A4a4E3e3O0o0T7t7L1l1";
 
-	for (i = 0; i < _strlen(a); i++)
+	for (i = 0; i < strlen(a); i++)
 	{
 		int j = 0;
 
-		for (; j < _strlen(ref); j++)
+		for (; j < 20; j++)
 		{
 			if (a[i] == ref[j])
 			{
 				a[i] = ref[j + 1];
-				j = 20;
+				break;
 			}
 		}
 	}
