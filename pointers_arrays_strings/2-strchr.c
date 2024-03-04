b7c38e2bec8fb788c9ca52ed48e046b162e7b1c7
root <seth.rueda@gmail.com>
2024-03-04 09:03:33 +0000

    add_strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - function that locates a character in a string
+ * @s: array where to locate c
+ * @c: character to locate in s
+ * Return: pointer p that point character adress
+ */ 
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+	char *p;
+	while (s[i] != c)
+	{
+		i++;
+	}
+	if (s[i] != c)
+		return NULL;
+	p = &s[i];
+	
+	return (p);
+}
