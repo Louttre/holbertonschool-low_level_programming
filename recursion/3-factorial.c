5d238a5877c2853061ab0db3b57b3b131affe6c7
root <seth.rueda@gmail.com>
2024-03-04 22:08:04 +0000

    add_factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - calcul factoriel de n
+ * @n: integer
+ * Return: factorial(n)
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n != 0)
+		return (n*factorial(n - 1));
+	else
+		return (1);
+}
