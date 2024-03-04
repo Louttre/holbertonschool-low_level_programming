b903e15722642aae2e5a6f4de71f49d69e9f9bfd
root <seth.rueda@gmail.com>
2024-03-04 11:53:11 +0000

    add_print_chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -7,13 +7,12 @@ void print_chessboard(char (*a)[8])
 {
 	unsigned int i,j = 0;
 
-	while (*a[i] != '\0')
+	for (; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
 		_putchar('\n');
-		i++;
 	}
 }
