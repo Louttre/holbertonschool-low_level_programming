a2d4d3c53f166694477983a30a175a58d172fb47
root <seth.rueda@gmail.com>
2024-03-04 10:07:06 +0000

    add_strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,7 @@
  * @s: array to check
  * @accept: substring
  * Return: number of bytes in the initial segment of s which consist only of bytes from accept
- */ 
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0;
