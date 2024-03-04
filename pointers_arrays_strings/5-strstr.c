c49ecb344ec7f221d7f1b43b9bb17b34f481a8df
root <seth.rueda@gmail.com>
2024-03-04 10:52:55 +0000

    add_strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strstr - function that searches a string for any of a set of bytes
+ * @haystack: string
+ * @needle: string
+ * Return: pointer to the byte in s, matches one of the bytes in accept
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0;
+	char *p = '\0';
+
+	while (haystack[i] != '\0')
+	{
+		unsigned int j = 0;
+
+		while (needle[j] != '\0')
+		{
+			if (haystack[i] == needle[j])
+			{
+				p = &s[i];
+
+				while (haystack[i] == needle[j])
+				{
+					i++;
+					j++;
+					if (needle[j] == '\0')
+					{
+						return (p);
+						break;
+					}
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	return (p);
+}
