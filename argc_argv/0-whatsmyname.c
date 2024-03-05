2dd2ec4147906bab958191a2f9f294d0005c9a1c
root <seth.rueda@gmailcom>
2024-03-05 12:20:32 +0000

    add_mynameis

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - print program name (argv[0])
+ * @argc: number of command line
+ * @argv: array of argc lenght
+ * Return: 0 
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
