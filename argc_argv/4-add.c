6d8f5c93fab48f8d87611f68715ca5911008f9a6
root <seth.rueda@gmail.com>
2024-03-05 17:06:36 +0000

    UPDATE add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
- * main - multiply all arg in argv
+ * main - add all arg in argv if it can
  * @argc: number of command line
  * @argv: array of argc lenght
- * Return: 0
+ * Return: 0, 1 if != digit
  */
 int main(int argc, char *argv[])
 {
 	int i = 1, res = 0;
+	size_t j;
+	char *car;
 
-	if (argc == 1)
+	if (argc < 1)
 	{
 		return (0);
 	}
 	for (; i < argc; i++)
 	{
-		if (atoi(argv[i]) == 0)
+		car = argv[i];
+		j = 0;
+		while (j < strlen(car))
 		{
-			printf("Error\n");
-			return (1);
+			if (car[j] < '0' || car[j] > '9')
+			{
+                        	printf("Error\n");
+                       		return (1);
+               		}
+			j++;
 		}
 		res += atoi(argv[i]);
-	}
+	}	
 	printf("%d\n", res);
 	return (0);
+	
 }
