5678a701c9267589e67cc874dabca9c6e650e5c0
root <seth.rueda@gmailcom>
2024-03-05 13:34:11 +0000

    add add program

---
+++ argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiply all arg in argv
+ * @argc: number of command line
+ * @argv: array of argc lenght
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, res = 1;
+
+	if (argc == 1)
+	{
+		return (0);
+	}
+	for (; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}	
+		res += atoi(argv[i]);
+	}
+	printf("%d\n", res);
+	return (0);
+}
