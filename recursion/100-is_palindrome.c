7423281277f2976c2e31285b3d6ea729794d6b35
root <seth.rueda@gmail.com>
2024-03-08 16:36:54 +0000

    UPDATE_is_palindrome

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -20,20 +20,21 @@ int _strlen(char *s)
  * @s: strinf to check
  * Return: 0 if false 1 if true
  */
+int check_palindrome(char *s,int i, int j)
+{
+	if (j <= i)
+		return (1);
+	else if (s[i] != s[j])
+		return (0);
+	else
+		return (check_palindrome(s, i + 1, j - 1));
+}
 int is_palindrome(char *s)
 {
 	int i = 0;
-	int j = _strlen(s) - 1;
-
-	while (i <= j)
-	{
-		if (s[i] == s[j])
-		{
-			i++;
-			j--;
-		}
-		else
-			return (0);
-	}
-	return (1);
+        int j = _strlen(s) - 1;
+	
+	if (s == NULL || _strlen(s) == 0)
+		return (1);
+	return (check_palindrome(s, i, j));
 }
