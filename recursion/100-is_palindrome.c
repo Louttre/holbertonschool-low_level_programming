e6a9c83ee84cb3f56fcf337c421bb710a6996738
root <seth.rueda@gmail.com>
2024-03-08 10:28:51 +0000

    add is_palindrme

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 /**
+ * _strlen - count number of case in array
+ * @s: string to count
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
  * is_palindrome - check if is s a palindrome
  * @s: strinf to check
  * Return: 0 if false 1 if true
@@ -8,7 +23,7 @@
 int is_palindrome(char *s)
 {
 	int i = 0;
-	int j = strlen(s) - 1;
+	int j = _strlen(s) - 1;
 
 	while (i <= j)
 	{
