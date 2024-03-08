c6ba168b22290eb92edb04c44078641141b2c8e7
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-08 17:40:57 +0100

    Update 100-is_palindrome.c

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -13,11 +13,13 @@ int _strlen(char *s)
 		return (_strlen(s + 1) + 1);
 }
 /**
- * is_palindrome - check if is s a palindrome
+ * check_palindrome - check if is s a palindrome
  * @s: strinf to check
+ * @i: 0
+ * @j: strlen(s)
  * Return: 0 if false 1 if true
  */
-int check_palindrome(char *s,int i, int j)
+int check_palindrome(char *s, int i, int j)
 {
 	if (j <= i)
 		return (1);
@@ -26,11 +28,16 @@ int check_palindrome(char *s,int i, int j)
 	else
 		return (check_palindrome(s, i + 1, j - 1));
 }
+/**
+ * is_palindrome - check if is s a palindrome
+ * @s: strinf to check
+ * Return: 0 if false 1 if true
+ */
 int is_palindrome(char *s)
 {
 	int i = 0;
-        int j = _strlen(s) - 1;
-	
+	int j = _strlen(s) - 1;
+
 	if (s == NULL || _strlen(s) == 0)
 		return (1);
 	return (check_palindrome(s, i, j));
