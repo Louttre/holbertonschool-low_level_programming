ea6b0b8662dd865fa9cce33baa0ca4360866a450
root <seth.rueda@gmail.com>
2024-03-09 22:44:39 +0000

    UPDATE_change

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -3,15 +3,19 @@
 int main(int  argc, char *argv[])
 {
 	int comptsus = 0;
-	int cents = atoi(argv[1]);
-	
-	if (argc > 2)
+	int cents;
+
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	cents = atoi(argv[1]);
 	if (cents < 0)
+	{
 		printf("0\n");
+		return (0);
+	}
 	while (cents - 25 >= 0)
 	{
 		cents -= 25;
