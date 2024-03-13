0e33aa09a9489693772da0a588c5f69c48a0b683
root <seth.rueda@gmail.com>
2024-03-13 12:24:05 +0000

    UPDATE

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,15 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-<<<<<<< HEAD
  * malloc_checked - allocates memory using malloc
  * @b: number of bytes
  * Return: a pointer to the allocated memory
-=======
- * malloc_checked - function that allocates memory
- * @b: unsigned int
- * Return: ptr
->>>>>>> refs/remotes/origin/main
  * if it fails, cause normal process termination with a status value of 98
  */
 void *malloc_checked(unsigned int b)
