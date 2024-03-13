f0de4e6c2ef35869e949721668a1d37feaaa6f68
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-13 09:07:47 +0100

    Update 4-free_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * free_grid - function that frees a 2 dimensional grid
+ * @grid: 2d array
+ * @height: height of the array
+ */
 void free_grid(int **grid, int height)
 {
 	int i = 0;
