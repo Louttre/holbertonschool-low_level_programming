606ff369a98dedacca8ded4ecd9d76cf8cba3981
root <seth.rueda@gmail.com>
2024-03-13 13:38:40 +0000

    UPDATE

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+  * init_dog - initialize struct dog
+  * @d: name of the struct
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: name of the owner
+  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
