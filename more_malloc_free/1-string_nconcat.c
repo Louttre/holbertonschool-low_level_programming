64f2dec4c0a18fcc6ac3ec68279feeeadaf14dc0
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-13 15:27:53 +0100

    Update 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -31,19 +31,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int j = 0;
 	char *a;
 	int len2 = _strlen(s2);
+	int len1 = _strlen(s1);
 	
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n < _strlen(s2))
+	if (n < len2)
 		len2 = n;
 	else
 		n = len2;
-	a = malloc(sizeof(char) * (_strlen(s1) + len2 + 1));
+	a = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (a == NULL)
 		return (NULL);
-	for (; i < _strlen(s1); i++)
+	for (; i < len1; i++)
 	{
 		a[i] = s1[i];
 	}
