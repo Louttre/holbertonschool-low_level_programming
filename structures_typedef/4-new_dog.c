c02c3d3e6d6ea6092748dbc36bc9355c7868e1c0
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-13 18:14:17 +0100

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+/**
+ * new_dog - instance a new dog.
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: pointer to new dog.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *NewDogo;
