3648601d2c5581767fec6d02e5bc70310df871cc
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-13 10:09:40 +0100

    Update 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,7 +13,9 @@ char *str_concat(char *s1, char *s2)
 	size_t i = 0;
 	size_t j = 0;
 	char *newstr;
-
+	
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
 	newstr = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 	if (newstr == NULL)
 		return (NULL);
