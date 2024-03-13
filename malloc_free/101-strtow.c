f8d460085d978d5bfc5cc4d070601be311e9a8ef
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-13 11:51:11 +0100

    Update 101-strtow.c

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+/**
+ * _strlen - count number of case in array
+ * @s: string to count
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 unsigned int comptword(char *str)
 {
 	size_t i = 0;
         unsigned int j = 0;
 	int comptword = 0;
 
-	for (; i < strlen(str); i++)
+	for (; i < _strlen(str); i++)
         {
                         if (str[j] != ' ' && str[j] != '\0')
                         {
@@ -28,11 +42,12 @@ char **strtow(char *str)
 	size_t i = 0;
 	unsigned int j = 0;
 	unsigned int k = 0;
+	int l = 0;
 	int comptletter = 0;
 	char **p;
 	
 	p = malloc(sizeof(char*) * (comptword(str) + 1));
-	for (; i < strlen(str); i++)
+	for (; i < _strlen(str); i++)
 	{
 		comptletter = 0;
 		while (str[j] == ' ')
@@ -46,6 +61,15 @@ char **strtow(char *str)
                 }
 		i = j;
 		p[k] = malloc(sizeof(char) * (comptletter + 1));
+		if (p == NULL)
+		{
+			for (; l < i; l++)
+				{
+					free(p[l]);
+				}
+				free(p);
+				return (NULL);
+		}
 		k++;
 	}
 	i = 0, j = 0, k = 0;
