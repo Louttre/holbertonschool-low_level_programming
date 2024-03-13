6ab1a0251dd472f98df37d0399a388c0b8735221
root <seth.rueda@gmail.com>
2024-03-13 08:44:56 +0000

    UPDATE strdup

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,18 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * _strdup - function that returns a pointer to a newly allocated space in memory
+ * _strdup - function that returns a pointer to a newly allocated space
  * @str: string to cpy
- * @Return: pointer to a copy of str or NULL
+ * Return: pointer to the new tab
  */
 char *_strdup(char *str)
 {
 	char *newstr;
 	size_t i = 0;
-	
+
 	if (str == NULL)
 		return (NULL);
-	newstr = (char *)malloc (sizeof(char) * (strlen(str) + 1));
+	newstr = malloc(sizeof(char) * (strlen(str) + 1));
 	while (i <= strlen(str))
 	{
 		newstr[i] = str[i];
