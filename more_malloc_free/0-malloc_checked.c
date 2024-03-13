e898432b947c73bdf7f63a9b2b4d07a0a0a66f20
root <seth.rueda@gmail.com>
2024-03-13 12:17:36 +0000

    UPDATE

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - function that allocates memory
+ * @b: unsigned int
+ * Return: ptr
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
