bbfbd1acce3cc96500820711616dc1e160f744c9
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-13 17:13:12 +0100

    Update 2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	for (; i < nmemb; i++)
+	for (; i < nmemb * size; i++)
 	{
 		p[i] = 0;
 	}
