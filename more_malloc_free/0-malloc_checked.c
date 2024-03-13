c95e701bab854ea6c702c017622a0da2b52eb413
root <seth.rueda@gmail.com>
2024-03-13 12:32:11 +0000

    UPDATE

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,4 @@
-#include “main.h”
+#include"main.h"
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -9,10 +9,9 @@
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);
-    if (ptr == NULL)
-    {
-        exit(98);
-    }
-    return (ptr);
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
 }
