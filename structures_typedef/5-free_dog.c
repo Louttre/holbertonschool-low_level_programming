467c8a39ebbc4d32556a09a292573e64eb8c666a
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-13 18:34:33 +0100

    Update 5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -5,10 +5,9 @@ void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		if (d->name)
-			free(d->name);
-		if (d->owner)
-			free(d->owner);
+		free(d->name);
+		free(d->owner);
+		free(d->age);
 		free(d);
 	}
 }
