cff7c771d3d2787141b53843520111d51cd75f02
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-13 14:15:38 +0100

    Update 3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -4,21 +4,15 @@
 
 int *array_range(int min, int max)
 {
-    	int size = max - min;
+    	int size = max - min + 1;
     	int *p = malloc(size * sizeof(int));
 	int i;	
     	
 	if (min > max) 
-	{
-        	return NULL;
-    	}
+        	return (NULL);
     	if (p == NULL) 
-	{
-        	return NULL;
-    	}
+        	return (NULL);
     	for (i = 0; i < size; i++) 
-	{
         	p[i] = min + i;
-    	}
     	return (p);
 }
