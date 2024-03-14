eb0600164a667289a1c59ef4ed3777dbbc82f8ae
root <seth.rueda@gmail.com>
2024-03-14 14:41:31 +0000

    UPDATE

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -15,60 +15,84 @@ int _strlen(char *s)
 	}
 	return (i);
 }
+/**
+ * comptword - count number of word in str
+ * @str: string
+ * Return: the number of word
+ */
 int comptword(char *str)
 {
 	int i = 0;
-        int j = 0;
+	int j = 0;
 	int comptword = 0;
 
 	for (; i < _strlen(str); i++)
-        {
-                        if (str[j] != ' ' && str[j] != '\0')
-                                comptword += 1;
-                        while (str[j] != ' ' && str[j] != '\0')
-                                j++;
-                        while (str[j] == ' ')
-                                j++;
-                        i = j;
-        }
+	{
+		if (str[j] != ' ' && str[j] != '\0')
+			comptword += 1;
+		while (str[j] != ' ' && str[j] != '\0')
+			j++;
+		while (str[j] == ' ')
+			j++;
+		i = j;
+	}
 	return (comptword);
 }
-char **strtow(char *str)
+/**
+ * allp - allocate memory for each word
+ * @p: array to stock pointers
+ * @str: string
+ * Return: the array with allocated memory
+ */
+char **allp(char **p, char *str)
 {
 	int i = 0;
 	int j = 0;
 	int k = 0;
 	int l = 0;
 	int comptletter = 0;
-	char **p;
-	
-	p = malloc(sizeof(char*) * (comptword(str) + 1));
-	if (p == NULL)
-		return (NULL);
+
 	for (; i < _strlen(str); i++)
 	{
 		comptletter = 0;
 		while (str[j] == ' ')
-                        j++;
+			j++;
 		while (str[j] != ' ' && str[j] != '\0')
-                {
-                        comptletter++;
-                        j++;
-                }
+		{
+			comptletter++;
+			j++;
+		}
 		i = j;
 		p[k] = malloc(sizeof(char) * (comptletter + 1));
 		if (p[k] == NULL)
 		{
 			for (; l < comptword(str); l++)
-					free(p[l]);
-				free(p);
-				return (NULL);
+				free(p[l]);
+			free(p);
+			return (NULL);
 		}
 		k++;
 	}
-	i = 0, j = 0, k = 0;
+	return (p);
+}
+/**
+ * strtow - function that splits a string into words
+ * @str: string to split
+ * Return: pointer to the string
+ */
+char **strtow(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	char **p;
+
+	p = malloc(sizeof(char *) * (comptword(str) + 1));
+	if (p == NULL)
+		return (NULL);
+	allp(p, str);
 	while (str[j] != '\0')
-	{		
+	{
 		while (str[j] == ' ')
 			j++;
 		while (str[j] != ' ' && str[j] != '\0')
