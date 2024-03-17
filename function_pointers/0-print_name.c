a2cb0f404552734d7d1afc4766b01430cf31e6b3
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-18 00:54:07 +0100

    Update 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: function to run
+ * Return: Nothing.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
