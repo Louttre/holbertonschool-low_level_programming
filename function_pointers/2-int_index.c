334f31081318ba17e125559cca5518b776d34f4b
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-18 01:16:30 +0100

    Update 2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -3,13 +3,13 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i;
+	int i;
 
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (cmp[i] == 1)
+		if (cmp(arrar[i]) == 1)
 			return (i);
 	}
 	if (i == size)
