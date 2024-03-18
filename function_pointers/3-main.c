35d8a5baada6cb04ae0c5e190e96b7b83aef7476
root <seth.rueda@gmail.com>
2024-03-18 11:04:48 +0000

    files for simple calculator

---
+++ function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * main - program that performs simple operations
+ * @argc: contain 2 int and a sign operator
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int calcul;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!get_op_func(argv[2]))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (argv[3][0] == '0' && (argv[2][0] == '%' || argv[2][0] == '/'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	calcul = (get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", calcul);
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * op_add - calcul addition
+ * @a: int
+ * @b: int
+ * Return: result of a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - calcul substraction
+ * @a: int
+ * @b: int
+ * Return: result of a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - calcul multiplication
+ * @a: int
+ * @b: int
+ * Return: result of a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - calcul division
+ * @a: int
+ * @b: int
+ * Return: result of a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - calcul modulo
+ * @a: int
+ * @b: b
+ * Return: result of a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
