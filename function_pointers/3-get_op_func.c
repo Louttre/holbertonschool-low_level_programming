a898e48d107d3f8b5b67f079137802c82e8a8394
root <seth.rueda@gmail.com>
2024-03-18 11:45:30 +0000

    UPDATE

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -11,17 +11,17 @@ int (*get_op_func(char *s))(int, int)
 	int i = 0;
 
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
 	while (ops[i].op != NULL)
 	{
 		if (strcmp(s, ops[i].op) == 0)
-				return (ops[i].f);
+			return (ops[i].f);
 		i++;
 	}
 	return (NULL);
