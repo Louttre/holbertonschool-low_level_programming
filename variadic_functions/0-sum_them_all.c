240a5c712ea9abcdd0315d496f70ea0e673e13b1
root <seth.rueda@gmail.com>
2024-03-19 10:00:52 +0000

    add header file and 0-sum_them_all

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - summarize all arguments
+ * @n: total of arguments
+ * Return: k result of the addition
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int j, k = 0;
+	unsigned int i;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		j = va_arg(args, int);
+		k += j;
+	}
+	va_end(args);
+	return (k);
+}
