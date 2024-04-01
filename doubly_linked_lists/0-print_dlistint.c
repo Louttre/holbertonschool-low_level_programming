0cf8c46b594074c0c4195cccc9268e80b39a86df
root <seth.rueda@gmail.com>
2024-04-01 13:17:46 +0000

    add 1-dlistint_len.c 0-print_dlistint.c and update lists.h

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while(h != NULL)
+	{
+		if (h->n)
+			printf("%d\n", h->n);
+		else
+			printf("(nil)\n");
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -17,5 +18,6 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
