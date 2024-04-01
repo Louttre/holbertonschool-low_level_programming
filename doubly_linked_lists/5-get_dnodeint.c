8e645a27cec38a35b8845ad32fe8615b313393ef
root <seth.rueda@gmail.com>
2024-04-01 21:59:56 +0000

    UPDATE

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *temp = head;
+
+	while (temp && count < index)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (temp);
+}
