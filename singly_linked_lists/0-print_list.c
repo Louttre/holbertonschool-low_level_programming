c1b51a50f61460b23df283ccdd1dc3f74b93b2e0
root <seth.rueda@gmail.com>
2024-04-01 00:47:12 +0000

    add 0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while(h != NULL)
+	{
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
