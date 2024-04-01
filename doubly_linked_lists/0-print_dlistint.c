7b43649f4dfa64604bc7485059c10aa706b1eacb
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-01 23:56:06 +0200

    Update 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,12 +8,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
+	if (!h)
+		return (0);
 	while(h != NULL)
 	{
-		if (h->n)
-			printf("%d\n", h->n);
-		else
-			printf("(nil)\n");
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
