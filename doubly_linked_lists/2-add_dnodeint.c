d97198d142b20b219d3fc9cb07d7a84849aaf442
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-02 02:08:15 +0200

    Update 2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -3,7 +3,12 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
-
+/**
+ * add_node_end - function that adds a new node at the start of a list
+ * @h: pointer to the head of the list
+ * @n: int
+ * Return: pointer to the head of the list
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
@@ -14,7 +19,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
-
 	new->next = *head;
 	new->prev = NULL;
 	*head = new;
