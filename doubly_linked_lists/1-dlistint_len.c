11fe2a27ac9c4c5336b1c97b8c44c1e3a9c78044
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-02 02:05:46 +0200

    Update 1-dlistint_len.c

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stddef.h>
-
+/**
+ * print_dlistint - function that returns the number of elements of the list
+ * @h: pointer to the head of the list
+ * Return: number of elements of the list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
