99f888f8c83ad7d7cc301d5b03fdcf37f2947f85
root <seth.rueda@gmail.com>
2024-04-02 15:17:02 +0000

    reverse

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,8 +15,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new;
 	unsigned int count = 0;
 
-	if (!h)
-		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
@@ -31,16 +29,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 	temp = *h;
 	count = 0;
-	while (temp && count < idx)
+	while (temp && count < idx - 1)
 	{
 		temp = temp->next;
 		count++;
 	}
-	if (count < idx || !temp)
+	if (count < idx - 1)
 		return (NULL);
-	new->prev = temp->prev;
+	new->prev = temp;
 	new->n = n;
-	new->next = temp;
-	temp->prev = new;
+	new->next = temp->next;
+	temp->next = new;
 	return (new);
 }
