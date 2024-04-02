6f3d8bcdf9f67739155114190cbd2b3896cf3fe3
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-02 02:05:00 +0200

    Update 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,11 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
-
+/**
+ * print_dlistint - function that prints all the elements of a list.
+ * @h: pointer to the head of the list
+ * Return: size of the list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
