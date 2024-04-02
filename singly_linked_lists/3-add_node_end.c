7776300207b849105fb4fc97ac3efdff386946a1
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-02 02:01:32 +0200

    Update 3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,7 +3,12 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
-
+/**
+ * aadd_node_end - function that adds a new node at the end of a list
+ * @h: pointer to the head of the list
+ * @str: string to duplicate
+ * Return: pointer to the tail of the list
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
