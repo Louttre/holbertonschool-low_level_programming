98c31aa321b8c12b42239880d93ad1ffdd0fc887
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-03 13:29:31 +0200

    Update 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (!idx)
 		return (add_dnodeint(h, n));
-	if (idx == count - 1)
+	if (idx == count)
 		return (add_dnodeint_end(h, n));
 	temp = *h;
 	count = 0;
