c95ecacd03114dd6aad5192a2836c3cc0e62cd80
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-03 12:19:58 +0200

    Update 2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,10 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new || !head)
-	{
-		free(new);
 		return (NULL);
-	}
 	new->n = n;
 	new->prev = NULL;
 	if (*head != NULL)
