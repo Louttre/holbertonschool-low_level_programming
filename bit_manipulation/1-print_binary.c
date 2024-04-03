0218310a8537cbcb93942fceff05afa3de5f5cb5
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-03 13:51:59 +0200

    Update 1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,5 +1,9 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: int
+ */
 void print_binary(unsigned long int n)
 {
 
