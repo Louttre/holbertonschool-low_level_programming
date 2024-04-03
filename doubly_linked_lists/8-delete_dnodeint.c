cc5f0d536f0c3f46238a68d87db374979dba004c
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-03 13:05:29 +0200

    Update 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -27,7 +27,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	while (temp && count < index)
 	{
-		if (temp = NULL)
+		if (!temp)
 			return (-1);
 		temp = temp->next;
 		count++;
