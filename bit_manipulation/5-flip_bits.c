f4f569656c6cae7ff6f0efcb3a29619849d14870
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-03 13:57:31 +0200

    Update 5-flip_bits.c

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * flip_bits - function that returns the number of bits to flip
+ * @n: The number
+ * @m: The number to flip n to
+ * Return: The necessary number of bits to flip to get from n to m.
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int i = 0;
