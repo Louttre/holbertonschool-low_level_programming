cd72710e0c09b4ec94ee8a416bb02d3be1cfa368
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-03 14:01:14 +0200

    Update 4-clear_bit.c

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -7,25 +7,23 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-        unsigned int i = 0;;
-        unsigned int j = 1;
-        int m;
-        int o;
-        
-        if (!n)
-            return (-1);
-        while (i < index)
-        {
-                j = j << 1;
-                i++;
-        }
-        m = *n ^ j;
-        o = m | j;
-        
-        if (o == *n)
-        {
-            *n = m;
-        }
-        return (1);
-            
+	unsigned int i = 0;
+	unsigned int j = 1;
+	int m;
+	int o;
+	
+	if (!n)
+		return (-1);
+	while (i < index)
+	{
+		j = j << 1;
+		i++;
+	}
+	m = *n ^ j;
+	o = m | j;
+	if (o == *n)
+	{
+		*n = m;
+	}
+	return (1);
 }
