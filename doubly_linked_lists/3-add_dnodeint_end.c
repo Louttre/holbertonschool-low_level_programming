77a08f37746df75057322d60b6da72b3db6636ba
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-03 11:16:15 +0200

    Update 3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -29,9 +29,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	last = *head;
 	while (last->next)
-	{
 		last = last->next;
-	}
 	last->next = new;
 	new->prev = last;
 	return (new);
