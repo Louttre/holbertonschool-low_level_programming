494b193e715f6d8d079fffa1a590be32745944c1
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-03 09:27:27 +0200

    Update 3-set_bit.c

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -6,11 +6,13 @@ int set_bit(unsigned long int *n, unsigned int index)
         unsigned int i = 0;;
         unsigned int j = 1;
 
+        if (!n)
+                return (-1);
         while (i < index)
         {
                 j = j << 1;
                 i++;
         }
         *n = *n | j;
-        return (*n);
+        return (1);
 }
