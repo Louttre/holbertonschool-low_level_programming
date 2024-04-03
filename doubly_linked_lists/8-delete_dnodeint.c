c7106dd752241cb3a684f1632bd48e0ac5e4eb51
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-03 11:40:46 +0200

    Update 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *temp2 = *head;
 	unsigned int count = 0;
 
-	if (!*head || !index)
+	if (!*head || !head)
 		return (-1);
 	if (index == 0)
 	{
