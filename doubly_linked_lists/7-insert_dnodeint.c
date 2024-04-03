9088e7b34a1de76759796a1a71a5e16332f21574
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-03 12:37:16 +0200

    Update 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -24,8 +24,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (!temp->next)
 		return (add_dnodeint_end(h, n));
-	if (count < idx - 1)
-		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
