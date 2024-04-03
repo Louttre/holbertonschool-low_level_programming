3c2b6f0c5474a2b8c21c603373c7e1db5bfe7126
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-04-03 10:52:47 +0200

    Update 5-flip_bits.c

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-        unsigned int i = 0;;
-        int o;
-        
+	unsigned int i = 0;
+	int o;
+
 	o = n ^ m;
-        while (o)
-        {
-            if (o & 1)
-                i++;
-            o = o >> 1;
-        }
-        return (i);
+	while (o)
+	{
+		if (o & 1)
+			i++;
+		o = o >> 1;
+	}
+	return (i);
 }
