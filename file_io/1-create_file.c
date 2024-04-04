cd51aec37ac1232d03f05fc2e1deac27eaf57606
root <seth.rueda@gmail.com>
2024-04-04 15:32:17 +0000

    now handle correctly if a null string is passed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -35,6 +35,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (!fd)
 		return (-1);
+	if (!text_content || *text_content == 0)
+	{
+		close(fd);
+		return (1);
+	}
 	n = write(fd, text_content, _strlen(text_content));
 	if (!n || n != _strlen(text_content))
 	{
