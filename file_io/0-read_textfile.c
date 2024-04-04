c8a4caea17f54fe6db1937855ad1b878b124b653
root <seth.rueda@gmail.com>
2024-04-04 11:58:38 +0000

    change write output

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -31,7 +31,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-	m = write(3, s, n);
+	m = write(STDOUT_FILENO, s, n);
 	if (!m || m != n)
 	{
 		free(s);
