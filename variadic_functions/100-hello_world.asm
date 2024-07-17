877ee70db2a361f3e483f2258ce9e995081444dd
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-17 18:41:13 +0200

    Create 100-hello_world.asm

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,17 @@
+section .data
+    str: db "Hello, World",0xa
+    strlen: equ $-str
+    
+section	.text
+    global _start
+
+  _start:
+    mov eax,4
+    mov ebx,1
+    mov ecx,str
+    mov edx,strlen
+    int 0x80
+    
+    mov eax,1
+    mov ebx,0
+    int 0x80
